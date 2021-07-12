2a8d84f11c700cc2beaa1535f65df1a7e9c65bcb
Louie Obaldo <louie.obaldo@education.wa.edu.au>
2021-07-12 12:31:55 +0800

    Updating server.js

--- Develop/public/assets/server.js
+++ Develop/public/assets/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path")
-const app = express();
+const server = express();
 const PORT = process.env.PORT || 3000;
+
+server.use(express.urlencoded({ extended: true}))
+server.use(express.json());
+
 
 
 
